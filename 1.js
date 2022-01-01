ff996ce78f5d9d26fe05963ac3c0ea66b3ca4adb
Luis E. Fraguada <fraguada@gmail.com>
2022-01-01 13:02:53 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,5 +1,21 @@
 let obj = new THREE.DodecahedronGeometry(1, 0);
-let mat = new THREE.MeshNormalMaterial({wireframe:true});
+//let mat = new THREE.MeshNormalMaterial({wireframe:true});
+let mat = new THREE.MeshStandardMaterial();
 let mesh = new THREE.Mesh(obj,mat);
-this.add(mesh);
+
+for ( var i = 0; i < 10000; i ++ ) {
+
+	var d = mesh.clone();
+	
+let x = Math.random() * 10;
+let y = Math.random() * 10;
+let z = Math.random() * 10;
+
+d.position.set(x,y,z);
+	this.add( d );
+
+
+}
+
+
 function update( event ) {}
