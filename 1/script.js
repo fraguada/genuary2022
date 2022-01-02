df5e4e6439d9a0eea311d8c5de33dec4b94cd168
Luis E. Fraguada <fraguada@gmail.com>
2022-01-02 19:42:46 +0100

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -1,10 +1,19 @@
-const obj = new THREE.DodecahedronGeometry(2, 0);
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.136.0/build/three.module.js'
+
+let scene, camera, renderer
+
+init()
+create()
+animate()
+
+function create() {
+  const obj = new THREE.DodecahedronGeometry(2, 0);
 //let mat = new THREE.MeshNormalMaterial({wireframe:true});
 const mat = new THREE.MeshStandardMaterial({flatShading:true, opacity:1});
 const mesh = new THREE.InstancedMesh(obj,mat, 10000);
 mesh.castShadow = true;
 mesh.receiveShadow = true;
-this.add(mesh);
+scene.add(mesh);
 //let temp = new THREE.Object3D();
 for ( let i = 0; i < 10000; i ++ ) {
 
@@ -21,6 +30,49 @@ for ( let i = 0; i < 10000; i ++ ) {
 
 }
 
+}
+
+function init() {
+
+  //renderer
+
+  renderer = new THREE.WebGLRenderer( { antialias: true } )
+  renderer.setPixelRatio( window.devicePixelRatio )
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  document.body.appendChild( renderer.domElement )
+
+  //scene
+
+  scene = new THREE.Scene()
+  scene.background = new THREE.Color( 1,1,1 )
+
+  //camera
+  
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 1, 1000 )
+  camera.position.z = 50
+
+  //lights
+  dLight = new THREE.DirectionalLight()
+  scene.add( dLight )
+
+  aLight = new THREE.AmbientLight()
+  scene.add( aLight )
+
+  window.addEventListener( 'resize', onWindowResize, false )
+
+}
+
+function animate () {
+  requestAnimationFrame( animate )
+  renderer.render( scene, camera )
+}
+  
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize( window.innerWidth, window.innerHeight )
+  animate()
+}
 
 function update( event ) {
 //mesh.rotation.y += 0.001;
