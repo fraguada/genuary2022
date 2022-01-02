cf45cc188e07b7d9a126884cd1694d08ce097932
Luis E. Fraguada <fraguada@gmail.com>
2022-01-02 07:21:57 +0100

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,40 +1,31 @@
-const obj = new THREE.DodecahedronGeometry(1, 0);
+const obj = new THREE.DodecahedronGeometry(2, 0);
 //let mat = new THREE.MeshNormalMaterial({wireframe:true});
-let mat = new THREE.MeshStandardMaterial({transparent:true, opacity:1});
-let mesh = new THREE.InstancedMesh(obj,mat, 10000);
+const mat = new THREE.MeshStandardMaterial({flatShading:true, opacity:1});
+const mesh = new THREE.InstancedMesh(obj,mat, 10000);
 mesh.castShadow = true;
-mesh.receiveShadow= true;
+mesh.receiveShadow = true;
 this.add(mesh);
 //let temp = new THREE.Object3D();
-for ( var i = 0; i < 10000; i ++ ) {
-	
-	
+for ( let i = 0; i < 10000; i ++ ) {
 
-	let temp = new THREE.Object3D();
-	
-let x = Math.random() * 10;
-let y = Math.random() * 10;
-let z = Math.random() * 10;
-	temp.position.set(x,y,z);
-	temp.updateMatrix(); 
-	mesh.setMatrixAt(i, temp.matrix);
-	
-	
-	
+  const temp = new THREE.Object3D();
 
+  const x = getRandomArbitrary(-10,10);
+  const y = getRandomArbitrary(-10,10);
+  const z = getRandomArbitrary(-10,10);
+  temp.position.set(x,y,z);
+  //temp.rotation.set(x,0,0);
 
-	
-	
-
+  temp.updateMatrix(); 
+  mesh.setMatrixAt( i, temp.matrix );
 
 }
-//this.add(g);
 
 
 function update( event ) {
-//g.rotation.x += 0.01;
-	//g.rotation.y += 0.01;
-	//g.rotation.z += 0.01;
+//mesh.rotation.y += 0.001;
+}
 
-
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
