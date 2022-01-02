3b3e789d7aefe29da444984283a58a32f8d5aa60
Luis E. Fraguada <fraguada@gmail.com>
2022-01-02 18:48:42 +0100

    Update 2.js

--- 2/2.js
+++ 2/2.js
@@ -1,5 +1,6 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.136.0/build/three.module.js'
-
+import { ToonShaderDotted } from 'https://cdn.jsdelivr.net/npm/three@0.136.0/examples/jsm/shaders/ToonShader.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.136.0/examples/jsm/controls/OrbitControls.js'
 init()
 
 function init() {}
